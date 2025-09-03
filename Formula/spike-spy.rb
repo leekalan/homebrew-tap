1b98dd1bbd2ec19b200abe1b5e84cb408d735289
leekalan <kalanjlee@gmail.com>
2025-09-03 14:27:37 +1000

    version update

--- Formula/spike-spy.rb
+++ Formula/spike-spy.rb
@@ -1,7 +1,7 @@
 class SpikeSpy < Formula
   desc "Tool for analyzing CPU usage spikes"
   homepage "https://github.com/leekalan/spike-spy"
-  url "https://github.com/leekalan/spike-spy/archive/refs/tags/v1.2.3.tar.gz"
+  url "https://github.com/leekalan/spike-spy/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "f98427c6201726fbaf2330415740f481bc470938aa1d79ad903a476c3034a33b"
   license "MIT"
 
